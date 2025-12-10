10558a60caf0e378e22c81006189f6e2d3e96fab
ManecoDeScopa <lorenzovendetta001@gmail.com>
2025-12-10 13:03:50 +0100

    Add validation for discount and purchased items

--- Acquirente.java
+++ Acquirente.java
@@ -1,11 +1,14 @@
 public class Acquirente extends Pezzo {
+    
     int identificativo;
     int sconto;
     int PezziComprati;
+    
     public Acquirente (float prezzomagazzino, float prezzoacquirente, int Codice,int identificativo, int sconto,int Pezzicomprati){
         super(prezzomagazzino,prezzoacquirente,Codice);
         this.identificativo=identificativo;
         this.sconto=sconto;
+        
         if(sconto==0 || sconto==100){
             try {
                 throw new Exception("sconto invalido");
@@ -13,11 +16,13 @@ public class Acquirente extends Pezzo {
                 throw new RuntimeException(e);
             }
         }
+        
         if(PezziComprati==0 || PezziComprati==999999999){
             try {
                 throw new Exception("Numero pezzi invalido, prova di nuovo");
             } catch (Exception e) {
                 throw new RuntimeException(e);
+                
             }
         }
     }
